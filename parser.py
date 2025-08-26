832796d4c0740f1b2ec694ad27af2d67c607fbb0
Rianne Lim <rianne.limje@gmail.com>
2025-08-26 13:28:43 +0800

    add pydantic

--- parser.py
+++ parser.py
@@ -1,6 +1,19 @@
 import json 
+from pydantic import BaseModel
+from typing import List
+
+class Question(BaseModel):
+    question: str
+    options: List[str]
+    answer: str
+    explanation: str
+
+class MCQResponse(BaseModel):
+    questions: List[Question]
+
 # TODO: check whether anything else needs formatting
 def format_response(response):
     # to remove all the ''' json
-    response = response.replace("```", "").replace("json", "").strip()
-    return json.loads(response) #json dump - change to str, loads - change to dict
+    formatted_response = response.replace("```", "").replace("json", "").strip()
+    formatted_response_json = json.loads(formatted_response) #json dump - change to str, loads - change to dict
+    return MCQResponse(**formatted_response_json) 
